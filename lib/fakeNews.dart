24eabefb6a3acdca8e9f1dcdcf6b1196711ede50
Mysinghshivam <mysinghshivam@gmail.com>
2024-05-31 13:41:43 +0530

    laptop

--- lib/fakeNews.dart
+++ lib/fakeNews.dart
@@ -21,104 +21,107 @@ class _FakeNewsState extends State<FakeNews> {
       body: Center(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-          child: Column(
-            children: [
-              Text("Enter the News Title, We Will Provide you Status Of News: ",
-              style: TextStyle(fontWeight: FontWeight.w500,fontSize: 15),),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: Colors.grey)),
-                child: TextField(
-                  controller: _fakeNewsTitle,
-                  decoration: InputDecoration(
-                    hintText: 'Enter Title Of News',
-                    contentPadding: EdgeInsets.all(15.0),
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.7),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Text("Enter the News Title, We Will Provide you Status Of News: ",
+                style: TextStyle(fontWeight: FontWeight.w500,fontSize: 15),),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey)),
+                  child: TextField(
+                    controller: _fakeNewsTitle,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Title Of News',
+                      contentPadding: EdgeInsets.all(15.0),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.7),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Example logic to update gauge
-                  bool? isRealNews = determineNewsType();
-                  updateGauge(isRealNews);
-                },
-                child: Text(
-                  'Submit',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green.withOpacity(0.9),
-                  elevation: 15,
-                  padding:
-                  EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Example logic to update gauge
+                    bool? isRealNews = determineNewsType();
+                    updateGauge(isRealNews);
+                  },
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green.withOpacity(0.9),
+                    elevation: 15,
+                    padding:
+                    EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 40.0),
-              SfRadialGauge(
-                axes: <RadialAxis>[
-                  RadialAxis(
-                    minimum: 0,
-                    maximum: 100,
-                    ranges: <GaugeRange>[
-                      GaugeRange(
-                          startValue: 0,
-                          endValue: 33.33,
-                          color: Colors.red.withOpacity(0.8),
-                          startWidth: 20, // Reduced width
-                          endWidth: 20, // Reduced width
-                          label: 'Fake News',
-                          labelStyle: GaugeTextStyle(fontSize: 15, color: Colors.white)
-                      ),
-                      GaugeRange(
-                          startValue: 33.33,
-                          endValue: 66.66,
-                          color: Colors.orange.withOpacity(0.8),
-                          startWidth: 20, // Reduced width
-                          endWidth: 20, // Reduced width
-                          label: 'Not Confirmed',
-                          labelStyle: GaugeTextStyle(fontSize: 15, color: Colors.white)),
-                      GaugeRange(
-                          startValue: 66.66,
-                          endValue: 100,
-                          color: Colors.green.withOpacity(0.8),
-                          startWidth: 20, // Reduced width
-                          endWidth: 20, // Reduced width
-                          label: 'Real News',
-                          labelStyle: GaugeTextStyle(fontSize: 15, color: Colors.white)),
-                    ],
-                    pointers: <GaugePointer>[
-                      NeedlePointer(value: _pointerValue),
-                    ],
-                    annotations: <GaugeAnnotation>[
-                      GaugeAnnotation(
-                        widget: Container(
-                          child: Text(
-                            'News Status',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
+                SizedBox(height: 30.0),
+                SfRadialGauge(
+                  axes: <RadialAxis>[
+                    RadialAxis(
+                      minimum: 0,
+                      maximum: 100,
+                      ranges: <GaugeRange>[
+                        GaugeRange(
+                            startValue: 0,
+                            endValue: 33.33,
+                            color: Colors.red.withOpacity(0.8),
+                            startWidth: 20, // Reduced width
+                            endWidth: 20, // Reduced width
+                            label: 'Fake News',
+                            labelStyle: GaugeTextStyle(fontSize: 15, color: Colors.white)
                         ),
-                        angle: 90,
-                        positionFactor: 0.9,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                        GaugeRange(
+                            startValue: 33.33,
+                            endValue: 66.66,
+                            color: Colors.orange.withOpacity(0.8),
+                            startWidth: 20, // Reduced width
+                            endWidth: 20, // Reduced width
+                            label: 'Not Confirmed',
+                            labelStyle: GaugeTextStyle(fontSize: 15, color: Colors.white)),
+                        GaugeRange(
+                            startValue: 66.66,
+                            endValue: 100,
+                            color: Colors.green.withOpacity(0.8),
+                            startWidth: 20, // Reduced width
+                            endWidth: 20, // Reduced width
+                            label: 'Real News',
+                            labelStyle: GaugeTextStyle(fontSize: 15, color: Colors.white)),
+                      ],
+                      pointers: <GaugePointer>[
+                        NeedlePointer(value: _pointerValue),
+                      ],
+                      annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                          widget: Container(
+                            child: Text(
+                              'News Status',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          angle: 90,
+                          positionFactor: 0.9,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/twitterBotPage.dart
+++ lib/twitterBotPage.dart
@@ -18,89 +18,94 @@ class _TwitterState extends State<Twitter> {
       body: Center(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-          child: Column(
-            children: [
-              SizedBox(height: 30),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: TextField(
-                  controller: _twitterIdController,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your Twitter ID',
-                    contentPadding: EdgeInsets.all(15.0),
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.7), // Change background color here
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Text("Enter the twiter Id, We Will Provide you Status Of Twitter Id: ",
+                  style: TextStyle(fontWeight: FontWeight.w500,fontSize: 15),),
+                SizedBox(height: 25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: _twitterIdController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your Twitter ID',
+                      contentPadding: EdgeInsets.all(15.0),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.7), // Change background color here
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Example logic to update gauge
-                  double twitterIdValue = determineTwitterIdValue();
-                  updateGauge(twitterIdValue);
-                },
-                child: Text(
-                  'Submit',
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green.withOpacity(0.9),
-                  elevation: 15,
-                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Example logic to update gauge
+                    double twitterIdValue = determineTwitterIdValue();
+                    updateGauge(twitterIdValue);
+                  },
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green.withOpacity(0.9),
+                    elevation: 15,
+                    padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 40.0),
-              SfRadialGauge(
-                axes: <RadialAxis>[
-                  RadialAxis(
-                    minimum: 1,
-                    maximum: 5,
-                    ranges: <GaugeRange>[
-                      GaugeRange(
-                        startValue: 1,
-                        endValue: 3,
-                        color: Colors.green.withOpacity(0.8),
-                        startWidth: 20, // Reduced width
-                        endWidth: 20, // Reduced width
-                        label: 'Real Twitter ID',
-                        labelStyle: GaugeTextStyle(fontSize: 12, color: Colors.black),
-                      ),
-                      GaugeRange(
-                        startValue: 3,
-                        endValue: 5,
-                        color: Colors.red.withOpacity(0.8),
-                        startWidth: 20, // Reduced width
-                        endWidth: 20, // Reduced width
-                        label: 'Fake Twitter ID',
-                        labelStyle: GaugeTextStyle(fontSize: 12, color: Colors.black),
-                      ),
-                    ],
-                    pointers: <GaugePointer>[
-                      NeedlePointer(value: _pointerValue),
-                    ],
-                    annotations: <GaugeAnnotation>[
-                      GaugeAnnotation(
-                        widget: Container(
-                          child: Text(
-                            'Twitter ID Status',
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
+                SizedBox(height: 20.0),
+                SfRadialGauge(
+                  axes: <RadialAxis>[
+                    RadialAxis(
+                      minimum: 1,
+                      maximum: 5,
+                      ranges: <GaugeRange>[
+                        GaugeRange(
+                          startValue: 1,
+                          endValue: 3,
+                          color: Colors.green.withOpacity(0.8),
+                          startWidth: 20, // Reduced width
+                          endWidth: 20, // Reduced width
+                          label: 'Real Twitter ID',
+                          labelStyle: GaugeTextStyle(fontSize: 12, color: Colors.black),
                         ),
-                        angle: 90,
-                        positionFactor: 0.9,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                        GaugeRange(
+                          startValue: 3,
+                          endValue: 5,
+                          color: Colors.red.withOpacity(0.8),
+                          startWidth: 20, // Reduced width
+                          endWidth: 20, // Reduced width
+                          label: 'Fake Twitter ID',
+                          labelStyle: GaugeTextStyle(fontSize: 12, color: Colors.black),
+                        ),
+                      ],
+                      pointers: <GaugePointer>[
+                        NeedlePointer(value: _pointerValue),
+                      ],
+                      annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                          widget: Container(
+                            child: Text(
+                              'Twitter ID Status',
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          angle: 90,
+                          positionFactor: 0.9,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
